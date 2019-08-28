9476e93ade113fa36c51876c470718ea3041b780
ph1p <phil@capshake.com>
2019-08-28 22:25:15 +0200

    Updates

---
+++ precache-manifest.b7e4388ceccf70213f8c590793025233.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7caf59a665ff6c50543",
+    "url": "bundle.js"
+  },
+  {
+    "revision": "20e3018926f32c7743b04bae7f33fb8a",
+    "url": "docker-compose.yml"
+  },
+  {
+    "revision": "415d6dad018489b9d071781bb174b569",
+    "url": "icons/android-icon-144x144.png"
+  },
+  {
+    "revision": "82f83edfed61bb7d3dc89b289f9fa9aa",
+    "url": "icons/android-icon-192x192.png"
+  },
+  {
+    "revision": "a35e79c7e035be19a93d590d957d932a",
+    "url": "icons/android-icon-36x36.png"
+  },
+  {
+    "revision": "8cee5f1e2f26e9071075a3c28ecaa7b4",
+    "url": "icons/android-icon-48x48.png"
+  },
+  {
+    "revision": "bb3fcfe3a8507c8da91e58adffc34996",
+    "url": "icons/android-icon-72x72.png"
+  },
+  {
+    "revision": "924c9baf157886c7781f752a859b2aae",
+    "url": "icons/android-icon-96x96.png"
+  },
+  {
+    "revision": "6c0bdded0386717f3e7efcd74f62d4ca",
+    "url": "icons/apple-icon-114x114.png"
+  },
+  {
+    "revision": "9a6f6ad2bfebfbbd41947b322e1047fe",
+    "url": "icons/apple-icon-120x120.png"
+  },
+  {
+    "revision": "a55888a557efe46d8f11ad06b511deba",
+    "url": "icons/apple-icon-144x144.png"
+  },
+  {
+    "revision": "d30aaa7d8eecb1207ad5727516748d17",
+    "url": "icons/apple-icon-152x152.png"
+  },
+  {
+    "revision": "e0cabc9514fc113198dce6ac5e8557ff",
+    "url": "icons/apple-icon-180x180.png"
+  },
+  {
+    "revision": "168ccd155bb606ac3f27707477277867",
+    "url": "icons/apple-icon-57x57.png"
+  },
+  {
+    "revision": "06a15c93681ce8db60866d91ff2028bb",
+    "url": "icons/apple-icon-60x60.png"
+  },
+  {
+    "revision": "bb3fcfe3a8507c8da91e58adffc34996",
+    "url": "icons/apple-icon-72x72.png"
+  },
+  {
+    "revision": "1b816e0fef25fdef8db8ed01a52172dc",
+    "url": "icons/apple-icon-76x76.png"
+  },
+  {
+    "revision": "82f83edfed61bb7d3dc89b289f9fa9aa",
+    "url": "icons/apple-icon-precomposed.png"
+  },
+  {
+    "revision": "5db1d2d997b5126e6ef06760313dcbb8",
+    "url": "icons/apple-icon.png"
+  },
+  {
+    "revision": "d942e4ce1d6d45fce4b2d028b0a79c2c",
+    "url": "icons/favicon-16x16.png"
+  },
+  {
+    "revision": "475c6e214c572053231b2bc162fbb2ec",
+    "url": "icons/favicon-32x32.png"
+  },
+  {
+    "revision": "924c9baf157886c7781f752a859b2aae",
+    "url": "icons/favicon-96x96.png"
+  },
+  {
+    "revision": "f532f2f76066ae2c8bd65021d864b32d",
+    "url": "icons/favicon.ico"
+  },
+  {
+    "revision": "415d6dad018489b9d071781bb174b569",
+    "url": "icons/ms-icon-144x144.png"
+  },
+  {
+    "revision": "b468360ff32010a34ade6ea4d05f8232",
+    "url": "icons/ms-icon-150x150.png"
+  },
+  {
+    "revision": "390d8941ab86f67144f81b2a88633020",
+    "url": "icons/ms-icon-310x310.png"
+  },
+  {
+    "revision": "0a3a09ea92a1592c6f38c4706ee3826a",
+    "url": "icons/ms-icon-70x70.png"
+  },
+  {
+    "revision": "41f3f9b8569147484145f4d7c02afeb0",
+    "url": "index.html"
+  },
+  {
+    "revision": "d7caf59a665ff6c50543",
+    "url": "main.css"
+  },
+  {
+    "revision": "ff60a8cd2ea582d783953c58d142e97e",
+    "url": "manifest.json"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,35 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "precache-manifest.b7e4388ceccf70213f8c590793025233.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/^https?.*/, new workbox.strategies.NetworkFirst({ "cacheName":"https-calls","networkTimeoutSeconds":15, plugins: [new workbox.expiration.Plugin({ maxEntries: 30, maxAgeSeconds: 2592000, purgeOnQuotaError: true }), new workbox.cacheableResponse.Plugin({ statuses: [ 0, 200 ] })] }), 'GET');
+workbox.routing.registerRoute(/^https:\/\/fonts\.gstatic\.com/, new workbox.strategies.CacheFirst({ "cacheName":"google-fonts-webfonts", plugins: [new workbox.cacheableResponse.Plugin({ statuses: [ 0, 200 ] }), new workbox.expiration.Plugin({ maxAgeSeconds: 31536000, purgeOnQuotaError: true })] }), 'GET');
